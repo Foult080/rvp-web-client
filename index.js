45f15859f9a8b7f273f13918217073027665b0ee
Vitaliy <foult080@gmail.com>
2021-08-30 18:16:41 +0700

    staging basic index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const fileUpload = require('express-fileupload');
+
+//use cors
+app.use(cors());
+//use json encode
+app.use(express.json({ extended: false }));
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
+
+
+//routes
+app.use("/api/client", require("./routes/client"));
+
+//resolve static folder for react app
+app.use(express.static("front-app/build"));
+//send index to client
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front-app", "build", "index.html"));
+});
+
+app.listen(5000, function () {
+  console.log("Listening on port 5000!");
+});
